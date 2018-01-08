5025c4889adce38861ebbbbb8c4755492c543ed9
yty <17600200818@163.com>
2018-01-08 14:19:49 +0800

    5.2

--- app/Http/Requests/TopicRequest.php
+++ app/Http/Requests/TopicRequest.php
@@ -27,7 +27,11 @@ class TopicRequest extends Request
             case 'DELETE':
             default:
             {
-                return [];
+                return [
+                        'title'       => 'required|min:2',
+                        'body'        => 'required|min:3',
+                        'category_id' => 'required|numeric',
+                ];
             };
         }
     }
@@ -35,7 +39,8 @@ class TopicRequest extends Request
     public function messages()
     {
         return [
-            // Validation messages
+            'title.min' => '标题必须至少两个字符',
+            'body.min' => '文章内容必须至少三个字符',
         ];
     }
 }
