4850e95679bc31b99736a8bfcfc514a81c6a991c
yty <17600200818@163.com>
2018-01-09 17:09:28 +0800

    6.3

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -3,6 +3,7 @@
 namespace App\Observers;
 
 use App\Models\Reply;
+use App\Notifications\TopicReplied;
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
@@ -18,9 +19,14 @@ class ReplyObserver
     public function created(Reply $reply)
     {
         $reply->topic->increment('reply_count', 1);
-//        $topic = $reply->topic;
+
+        $topic = $reply->topic;
+        if ( !$reply->user->isAuthorOf($topic) ) {
+            $topic->user->notify(new TopicReplied($reply));
+        }
 //        $topic->reply_count++;
 //        $topic->save();
+
     }
 
     public function updating(Reply $reply)
